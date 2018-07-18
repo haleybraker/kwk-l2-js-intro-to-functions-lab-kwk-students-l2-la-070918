448ef377d9652d1a7d85d2ca1822ab06d70f400b
haleybraker <haleybraker@gmail.com>
2018-07-18 22:36:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 // Write your code in this file!
 
+//const currentHour = new Date().getHours()
 
+//???????? I DONNT KNO W WHAT IKM DOIGN 
+
+function getUKTime () {
+  new Date().getHours() + 5
+  return Date().getHours() + 5
+}
+
+getUKTime()
